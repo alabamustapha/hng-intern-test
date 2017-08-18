51965a20cbc96b197d162f3081a55da2d6354400
alabamustapha <alabamustapha@gmail.com>
2017-08-18 19:37:12 +0100

    read from db to display posts

--- index.php
+++ index.php
@@ -11,5 +11,12 @@ $post = posts(2);
 
 $posts = posts([1,2,3,4,5]);
 
+while ($post = mysqli_fetch_array($posts)) {
+	
+	echo "<h2>" . $post['title']. "</h2>";
+	echo "<p>" 	. $post['body']. "</p>";
+	
+}
+
 
 
