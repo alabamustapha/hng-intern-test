a3ba8a6f233b8884b1d6f9682b08cf75dbb21420
alabamustapha <alabamustapha@gmail.com>
2017-08-18 19:41:53 +0100

    pull and push

--- index.php
+++ index.php
@@ -5,17 +5,17 @@ require_once 'functions.php';
  * reading from post table
  */
 
-$all_posts = posts();
+$posts = posts();
 
-$post = posts(2);
+//$post = posts(2); //for a single post
 
-$posts = posts([1,2,3,4,5]);
+//$posts = posts([1,2,3,4,5]); //for some posts
 
 while ($post = mysqli_fetch_array($posts)) {
 	
 	echo "<h2>" . $post['title']. "</h2>";
 	echo "<p>" 	. $post['body']. "</p>";
-	
+
 }
 
 
